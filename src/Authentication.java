ad8c9046dd31f4881c9d99ebdc555f32cd3dc879
Muntahaa Khan <103258353+Pasha-007@users.noreply.github.com>
2024-11-03 00:02:42 +0600

    Authentication

---
+++ src/Authentication.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Authentication {
+    private HashMap<String, String> users = new HashMap<String, String>();
+    private Scanner scanner = new Scanner(System.in);
+    private final String filePath = "src/users.txt";
+
+    public Authentication() {
+        loadUsers();
+    }
+
+    private void loadUsers(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if(parts.length == 2){
+                    users.put(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveUser(String name, String password){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))){
+            writer.write(name+ "," + password);
+            writer.newLine();
+        } catch (IOException e) {
+            System.out.println("Error saving user: " + e.getMessage());
+        }
+    }
+
+    public void Signup(){
+        System.out.println("You can Signup");
+        System.out.println("Enter name");
+        String name = scanner.nextLine();
+        System.out.println("Enter password");
+        String password = scanner.nextLine();
+        users.put(name, password);
+        saveUser(name, password);
+        Login();
+    }
+    public void Login(){
+        System.out.println("You can Login");
+        System.out.println("Enter Name: ");
+        String input = scanner.nextLine();
+        System.out.println("Enter Pass: ");
+        String input2 = scanner.nextLine();
+        if(users.containsKey(input) && users.get(input).equals(input2)){
+            System.out.println("You are logged in");
+        }else{
+            System.out.println("Ivalid Username or Password");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String action;
+
+        Authentication auth = new Authentication();
+
+        while(true) {
+            System.out.println("Login or Signup");
+            action = scanner.nextLine();
+            if(action.equalsIgnoreCase("Login")){
+                auth.Login();
+                break;
+            } else if (action.equalsIgnoreCase("Signup")) {
+                auth.Signup();
+                break;
+            }else {
+                System.out.println("Invalid choice. Please type 'Login' or 'Signup.'");
+            }
+        }
+        scanner.close();
+    }
+}
